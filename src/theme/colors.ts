e773df5d854f28be4dd27cc8aabcb5045209eac8
Pedro Alonso <pedro.alonsodeoliveira@hotmail.com>
2025-04-08 14:31:50 -0300

    💄 Updated theme usage

---
+++ src/theme/colors.ts
@@ -0,0 +1,20 @@
+const colors = {
+  neutral: {
+    lightest: "#f9f9f9",
+    light: "#e0e0e0",
+    regular: "#b0b0b0",
+    dark: "#707070",
+    darkest: "#303030",
+  },
+  primary: {
+    lightest: "#ffc6bf",
+    light: "#dba39c",
+    regular: "#b04e43",
+    dark: "#803830",
+    darkest: "#332826",
+  },
+  background: "linear-gradient(to bottom, rgb(244, 241, 236) 0%, white 100%)",
+  foreground: "#303030",
+};
+
+export default colors;
